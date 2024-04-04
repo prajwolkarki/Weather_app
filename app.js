18a3e7dc4a7ae12a776672f5cd1e99789b873f9e
Prajwol <prajwolkarki609@gmail.com>
2024-04-04 19:25:05 +0545

    Added all files

---
+++ app.js
@@ -0,0 +1,31 @@
+const apiKey = "80edcbffb13bd2a5bff9604eddf47a1f";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector('.weather-icon')
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = data.main.temp + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + "Km/hr";
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "images/snow.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
